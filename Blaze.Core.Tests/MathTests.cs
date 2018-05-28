7a723bad8a31fc13f60bc00ab2fb78ab7387d1bf
Gabriel Belmonte <gabriel.belmonte@ubisoft.com>
2018-05-28 18:23:20 -0400

    Added GTest and tried it out

--- Blaze.Core.Tests/MathTests.cs
+++ Blaze.Core.Tests/MathTests.cs
@@ -27,6 +27,19 @@ namespace Blaze.Core.Tests
 
             occurrances = new List<int>
             {
+                10,
+                10,
+                10,
+                10,
+                10,
+                10
+            };
+
+            cs = Stats.ChiSquared(occurrances);
+            Assert.That.AreEqualWithEpsilon(0, cs);
+
+            occurrances = new List<int>
+            {
                 60,
                 0,
                 0,
@@ -42,6 +55,67 @@ namespace Blaze.Core.Tests
             cs = Stats.ChiSquared(occurrances);
 
             Assert.That.AreEqualWithEpsilon(300.0, cs);
+        }
+
+        [TestMethod]
+        public void TestGTest()
+        {
+            //Uniform distribution
+            var occurrances = new List<int>
+            {
+                10,
+                10,
+                10,
+                10,
+                10,
+                10
+            };
+
+            double cs = Stats.GTest(occurrances);
+            Assert.That.AreEqualWithEpsilon(0, cs);
+
+            //Totally skewed distribution
+            occurrances = new List<int>
+            {
+                60,
+                0,
+                0,
+                0,
+                0,
+                0
+            };
+
+            cs = Stats.GTest(occurrances);
+            const double run = 215.0111363;
+            Assert.That.AreEqualWithEpsilon(run, cs, 0.001);
+
+            //Ten times more occurrances
+            occurrances = new List<int>
+            {
+                600,
+                0,
+                0,
+                0,
+                0,
+                0
+            };
+
+            cs = Stats.GTest(occurrances);
+            Assert.That.AreEqualWithEpsilon(run*10, cs, 0.001);
+
+            // Evil Dice distribution
+            occurrances = new List<int>
+            {
+                5,
+                8,
+                9,
+                8,
+                10,
+                20
+            };
+
+            cs = Stats.GTest(occurrances);
+            Assert.That.AreEqualWithEpsilon(11.757, cs, 0.001);
         }
     }
 }
