107915003d98f4171c687a486b2b28ce24cf3b38
Gabriel Belmonte <gabriel.belmonte@ubisoft.com>
2018-06-01 18:28:13 -0400

    SaltedCypher

---
+++ Blaze.Encryption/SaltedCypher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blaze.Core.Collections;
+using Blaze.Core.Extensions;
+using Blaze.Cryptography.Rng;
+
+namespace Blaze.Cryptography
+{
+    public class SaltedCypher<TCypher> : AlphabeticCypher where TCypher : ICypher
+    {
+        ICypher _cypher;
+        public SaltedCypher(ICypher inner)
+        {
+            _cypher = inner;
+        }
+
+        protected override byte[] Encrypt(byte[] plain, byte[] key, Op op)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
